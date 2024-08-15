3884e25e8279a63811a419423097e4cd5fe41478
Junlee <junleearaujo17@gmail.com>
2024-08-15 18:03:47 -0300

    Cart

--- src/ui/pages/CartPage/CartPage.jsx
+++ src/ui/pages/CartPage/CartPage.jsx
@@ -1,52 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '@context/CartContext';
+import CartItem from '@components/CartItem/CartItem';
+import CartSummary from '@components/CartSummary/CartSummary';
 import '@styles/pages/Cartpage/Cartpage.css';
 
-const CartPage = ({ carrinho = [], removerItem, atualizarQuantidade }) => {
-  const calcularTotal = () => {
-    return carrinho.reduce((total, item) => total + item.preco * item.quantidade, 0).toFixed(2);
-  };
+function CartPage() {
+  const { cart } = useContext(CartContext);
+
+  const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return (
     <div className="cart-page">
-      <h1>Meu Carrinho</h1>
-      <div className="cart-items">
-        {carrinho.length === 0 ? (
-          <p>Seu carrinho está vazio.</p>
-        ) : (
-          carrinho.map((item, index) => (
-            <div key={index} className="cart-item">
-              <img src={item.imagem} alt={item.nome} className="cart-item-imagem" />
-              <div className="cart-item-detalhes">
-                <h2>{item.nome}</h2>
-                <p>Preço: R${item.preco.toFixed(2)}</p>
-                <label>
-                  Quantidade:
-                  <input
-                    type="number"
-                    min="1"
-                    value={item.quantidade}
-                    onChange={(e) => atualizarQuantidade(index, parseInt(e.target.value))}
-                  />
-                </label>
-                <button
-                  className="remover-item-btn"
-                  onClick={() => removerItem(index)}
-                >
-                  Remover
-                </button>
-              </div>
-            </div>
-          ))
-        )}
-      </div>
-      <div className="cart-summary">
-        <h2>Total: R${calcularTotal()}</h2>
-        <button className="finalizar-compra-btn">
-          Finalizar Compra
-        </button>
-      </div>
+      <h1>Seu Carrinho</h1>
+      {cart.length > 0 ? (
+        <>
+          <div className="cart-items">
+            {cart.map((item, index) => (
+              <CartItem key={index} item={item} />
+            ))}
+          </div>
+          <CartSummary totalPrice={totalPrice} />
+        </>
+      ) : (
+        <p>Seu carrinho está vazio.</p>
+      )}
     </div>
   );
-};
+}
 
 export default CartPage;
